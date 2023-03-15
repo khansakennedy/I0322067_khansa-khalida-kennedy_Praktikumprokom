34ce5b849be90d5988ff12b0fbd62211f0bfb352
Khansa Khalida Kennedy <khansakennedy11@gmail.com>
2023-03-16 01:50:33 +0700

    Mengupload exercise 01

---
+++ Exercise-01.py
@@ -0,0 +1,5 @@
+print ("Nama : Khansa Khalida Kennedy")
+print ("NIM  : I0322067")
+print ("Asal : Yogyakarta")
+print ("Tempat tinggal saya salah satu provinsi yang dikenal sebagai Kota wisata, tetapi UMR atau upah minimum yang dihasilkan kota yogyakarta paling rendah dari seluruh provinsi di Jawa. Yogyakarta juga terkenal sebagai kota pelajar karena banyak sekali sekolah sampai universitas yang ada di sini")
+print ("Setelah saya lulus dari Teknik Industri UNS, saya berharap bisa menjadi orang yang bermanfaat bagi sekitar saya dan bisa mencapai apa yang saya cita-citakan")
